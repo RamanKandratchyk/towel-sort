8cc5a03c93fdda07b16d83e5d5b3bddec21e432d
Raman Kandratchyk <kondrator@gmail.com>
2022-07-13 19:06:42 +0300

    init: add OWN solution in index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+module.exports = function towelSort(matrix) {
 
-// You should implement your task here.
+  if (arguments.length === 0) return [];
 
-module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix.length === 0) return [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 !== 0) { matrix[i].reverse() };
+  }
+
+  return matrix.join(',').split(',').map(Number);
 }
